63abbb11995f8c8060579b924ec1e0cc7aeba698
Collins Abitekaniza <abtcolns@gmail.com>
2017-09-26 05:18:14 +0300

    Fix ball movement and direction

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+const SPEED_UP = 400
+const MAX_SPEED = 30000
+
 func _ready():
 	set_fixed_process(true)
 	
@@ -8,3 +11,8 @@ func _fixed_process(delta):
 	for body in bodies:
 		if body.is_in_group("Bricks"):
 			body.queue_free()
+		if body.get_name() == "Paddle":
+			var speed = get_linear_velocity().length()
+			var direction = get_pos() - body.get_node("Anchor").get_global_pos()
+			var velocity = direction.normalized()*min(speed+SPEED_UP*delta, MAX_SPEED*delta)
+			set_linear_velocity(velocity)
